68345229403c50f744f88e988ba0c38a021448be
ruscoe <danruscoe@gmail.com>
2020-04-21 01:04:07 -0700

    Add initial plugin file.

---
+++ wp-comics.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WordPress Comics
+ * Plugin URI: https://github.com/ruscoe/wp-comics/
+ * Description: Catalogue your comic book collection in WordPress!
+ * Author: Dan Ruscoe
+ * Author URI: http://ruscoe.org/
+ * Version: 1.0.0
+ * Text Domain: wordpress-comics
+ * Domain Path: /i18n/languages/
+ *
+ * License: MIT License
+ * License URI: https://en.wikipedia.org/wiki/MIT_License
+ *
+ * @author   Dan Ruscoe (dan@ruscoe.org)
+ * @license  https://en.wikipedia.org/wiki/MIT_License MIT License
+ */
+
+defined( 'ABSPATH' ) or exit;
